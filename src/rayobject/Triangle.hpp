68f05854edc79b933da2c5f8bbf8955097f48912
Hamza-EL-ACHHAB <hamza.elachhab@usmba.ac.ma>
2025-11-06 15:15:58 +0100

    feat: Add triangle rendering with materials

---
+++ src/rayobject/Triangle.hpp
@@ -0,0 +1,18 @@
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+#include "Object.hpp"
+#include "../raymath/Vector3.hpp"
+#include "../raymath/Material.hpp"
+
+class Triangle : public Object {
+public:
+    Triangle(const Vector3& vertex0, const Vector3& vertex1, const Vector3& vertex2, const Material& material);
+    bool intersect(const Ray& ray, float tMin, float tMax, HitRecord& hitRecord) const override;
+
+public:
+    Vector3 vertex0, vertex1, vertex2;
+    Material material;
+};
+
+#endif // TRIANGLE_HPP
